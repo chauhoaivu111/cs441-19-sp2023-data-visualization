566d202fdb5f63a44717f6254c80e82a74a35ad3
your <@chauhoaivu111@gmail.com>
2023-05-01 20:23:26 +0700

    ad core

--- server.js
+++ server.js
@@ -1,13 +1,62 @@
+// const express = require("express");
+// const axios = require("axios");
+// const cheerio = require("cheerio");
+
+// const app = express();
+
+// app.get("/", async (req, res) => {
+//   try {
+//     const response = await axios.get("https://www.worldometers.info/coronavirus/", { timeout: 60000 });
+//     const $ = cheerio.load(response.data);
+
+//     // Get the table data
+//     const data = [];
+//     const table = $("#main_table_countries_today");
+//     const rows = table.find("tr");
+//     const headers = [];
+//     $(rows[0]).find("th").each((i, header) => {
+//       headers.push($(header).text().trim());
+//     });
+//     rows.each((i, row) => {
+//       const cols = $(row).find("td");
+//       const rowData = {};
+//       cols.each((j, col) => {
+//         rowData[headers[j]] = $(col).text().trim();
+//       });
+//       if (Object.keys(rowData).length > 0) {
+//         data.push(rowData);
+//       }
+//     });
+  
+//     res.json(data);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Error fetching data");
+//   }
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+
+
+
+
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://www.worldometers.info/coronavirus/", { timeout: 60000 });
     const $ = cheerio.load(response.data);
+    const title = $("title").text();
 
     // Get the table data
     const data = [];
