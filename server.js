de9497b5c263e8c79dac7aa487c0be3c765cd6cd
your <@chauhoaivu111@gmail.com>
2023-05-01 20:05:37 +0700

    new code

--- server.js
+++ server.js
@@ -9,9 +9,44 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://www.worldometers.info/coronavirus/", { timeout: 60000 });
     const $ = cheerio.load(response.data);
-    const title = $("title").text();
 
-    res.send(`Title: ${title}`);
+    // Get the table data
+    const data = [];
+    $('#main_table_countries_today tbody tr').each((i, row) => {
+      if (i === 0) {
+        // Skip the header row
+        return;
+      }
+      const cells = $(row).find('td');
+      const country = $(cells[0]).text().trim();
+      const totalCases = $(cells[1]).text().trim();
+      const newCases = $(cells[2]).text().trim();
+      const totalDeaths = $(cells[3]).text().trim();
+      const newDeaths = $(cells[4]).text().trim();
+      const totalRecovered = $(cells[5]).text().trim();
+      const activeCases = $(cells[6]).text().trim();
+      const criticalCases = $(cells[7]).text().trim();
+      const casesPerMillion = $(cells[8]).text().trim();
+      const deathsPerMillion = $(cells[9]).text().trim();
+      const tests = $(cells[10]).text().trim();
+      const testsPerMillion = $(cells[11]).text().trim();
+      data.push({
+        country,
+        totalCases,
+        newCases,
+        totalDeaths,
+        newDeaths,
+        totalRecovered,
+        activeCases,
+        criticalCases,
+        casesPerMillion,
+        deathsPerMillion,
+        tests,
+        testsPerMillion,
+      });
+    });
+
+    res.json(data);
   } catch (err) {
     console.error(err);
     res.status(500).send("Error fetching data");
@@ -33,25 +68,25 @@ app.listen(port, () => {
 //   const browser = await puppeteer.launch();
 //   const page = await browser.newPage();
 
-//   await page.goto('https://www.worldometers.info/coronavirus/');
+  // await page.goto('https://www.worldometers.info/coronavirus/');
 
-//   // Wait for the table to load
-//   await page.waitForSelector('#main_table_countries_today');
+  // // Wait for the table to load
+  // await page.waitForSelector('#main_table_countries_today');
 
-//   // Get the table data
-//   const data = await page.evaluate(() => {
-//     const table = document.querySelector('#main_table_countries_today');
-//     const rows = table.querySelectorAll('tr');
-//     const headers = Array.from(rows[0].querySelectorAll('th')).map(th => th.innerText.trim());
-//     const values = Array.from(rows).slice(1).map(row => {
-//       const cells = Array.from(row.querySelectorAll('td')).map(td => td.innerText.trim());
-//       return cells.reduce((result, cell, index) => {
-//         result[headers[index]] = cell;
-//         return result;
-//       }, {});
-//     });
-//     return values;
-//   });
+  // // Get the table data
+  // const data = await page.evaluate(() => {
+  //   const table = document.querySelector('#main_table_countries_today');
+  //   const rows = table.querySelectorAll('tr');
+  //   const headers = Array.from(rows[0].querySelectorAll('th')).map(th => th.innerText.trim());
+  //   const values = Array.from(rows).slice(1).map(row => {
+  //     const cells = Array.from(row.querySelectorAll('td')).map(td => td.innerText.trim());
+  //     return cells.reduce((result, cell, index) => {
+  //       result[headers[index]] = cell;
+  //       return result;
+  //     }, {});
+  //   });
+  //   return values;
+  // });
 
 //   await browser.close();
 
